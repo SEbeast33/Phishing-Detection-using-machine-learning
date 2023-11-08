aef1101fa959d515aabae5524b56e5f2d9b6f456
sharad <sharadetthar810@gmai.com>
2023-11-09 00:09:42 +0530

    final touch

--- phishing_detection/phishing_detecter/models.py
+++ phishing_detection/phishing_detecter/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class SubmittedURL(models.Model):
-    url = models.URLField(max_length=200)
+    url = models.CharField(max_length=4000)
     is_phishing = models.BooleanField(default=False)
